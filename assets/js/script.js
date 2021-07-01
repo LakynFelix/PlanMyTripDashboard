d67b53b59937e6dcda5fcd74047d2cb55dce369d
Lakyn <lakynmphotography@hotmail.com>
2021-07-01 01:45:47 -0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 var cityEl = document.querySelector("#user-city");
-var getcity = function(city) {
+var cityInputEl = document.querySelector("#city");
+
+var formSubmitHandler = function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+  
+    // get value from input element
+    var city = cityInputEl.value.trim();
+  
+    if (city) {
+      getCity(city);
+    } 
+  };
+
+
+var getCity = function(city) {
    
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}" + user + "/repos";
   
