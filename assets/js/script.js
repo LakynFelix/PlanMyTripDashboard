020ed4059bac7b120d108395692c50c572070c16
Lakyn <lakynmphotography@hotmail.com>
2021-07-03 21:32:45 -0400

    images

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,8 @@ var getCity = function(city) {
    
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}" + user + "/repos";
   
-    // make a get request to url
     fetch(apiUrl)
       .then(function(response) {
-        // request was successful
         if (response.ok) {
           console.log(response);
           response.json().then(function(data) {
