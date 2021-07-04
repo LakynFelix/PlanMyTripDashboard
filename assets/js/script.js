58ecf2fcad59596db3d3c165cf4cfff68faf28f3
Lakyn <lakynmphotography@hotmail.com>
2021-07-04 00:07:49 -0400

    changes: git commit -mchanges git commit -mchanges:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 var cityEl = document.querySelector("#user-city");
 var cityInputEl = document.querySelector("#city");
+
 const currentPicEl = document.getElementById("img");
 const currentTempEl = document.getElementById("temp");
 const currentHumidityEl = document.getElementById("humidity");
-const currentWindEl = document.getElementById("wind-speed");
+const currentmntWindEl = document.getElementById("wind-speed");
 const currentUVEl = document.getElementById("UV-index");
+var cityContainerEl = document.querySelector("#repos-container");
+var SearchTerm = document.querySelector("#repo-search-term");
+
 const APIKey = "84b79da5e5d7c92085660485702f4ce8";
 
 
 var formSubmitHandler = function(event) {
-    // prevent page from refreshing
+ 
     event.preventDefault();
   
     // get value from input element
@@ -17,14 +21,20 @@ var formSubmitHandler = function(event) {
   
     if (city) {
       getCity(city);
-    } 
+  
+    repocContainerEl.textContent = "";
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
   };
 
+  
 
 var getCity = function(city) {
    
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}" + city ;
-  
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + APIKey;
+
     fetch(apiUrl)
       .then(function(response) {
         if (response.ok) {
