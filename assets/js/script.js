b60767e20ec9c76e99bef92b25b99cfa47d96444
Lakyn <lakynmphotography@hotmail.com>
2021-07-04 02:26:10 -0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,8 @@ const currentHumidityEl = document.getElementById("humidity");
 const currentmntWindEl = document.getElementById("wind-speed");
 const currentUVEl = document.getElementById("UV-index");
 var cityContainerEl = document.querySelector("#forcast-container");
-var SearchTerm = document.querySelector("#search");
-var cuttentDate 
+var searchTerm = document.querySelector("#search");
+
 const APIKey = "84b79da5e5d7c92085660485702f4ce8";
 
 
@@ -32,9 +32,7 @@ var formSubmitHandler = function(event) {
   
 
 var getCity = function(city) {
-   
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + APIKey;
-
     fetch(apiUrl)
       .then(function(response) {
         if (response.ok) {
@@ -45,15 +43,40 @@ var getCity = function(city) {
           });
        
   };
+      }
+    ,
 
+      // Daily forcast//
+      const currentDate = new Date(response.data.dt * 1000);
+      const day = currentDate.getDate();
+      const month = currentDate.getMonth() + 1;
+      const year = currentDate.getFullYear();
+      nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
 
  // Get 5 day forecast  //
+ const forecastEls = document.querySelectorAll(".forecast");
+ for (i = 0; i < forecastEls.length; i++) {
+     forecastEls[i].innerHTML = "";
+     const forecastIndex = i * 8 + 4;
+     const forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+     const forecastDay = forecastDate.getDate();
+     const forecastMonth = forecastDate.getMonth() + 1;
+     const forecastYear = forecastDate.getFullYear();
+     const forecastDateEl = document.createElement("p");
+     forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+     forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+     forecastEls[i].append(forecastDateEl);
 
   // Get history from local storage //
+
   searchEl.addEventListener("click", function () {
     const searchTerm = cityEl.value;
     getWeather(searchTerm);
     searchHistory.push(searchTerm);
     localStorage.setItem("search", JSON.stringify(searchHistory));
     renderSearchHistory();
-})
+});
+
+
+// add event listeners 
+cityEl.addEventListener("search", formSubmitHandler);
